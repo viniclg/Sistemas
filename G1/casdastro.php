e26ca7a1e87f005e7b6552ba853a0f1e8f522c61
bardo4life <124942056+bardo4life@users.noreply.github.com>
2023-02-09 20:57:00 -0300

    Add files via upload

---
+++ G1/casdastro.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+
+<h1>BEM VINDO AO SITE</h1>
+
+<form method='post' action='inserir.php' id='formlogin' name='formlogin'>
+<fieldset id='fie'>
+
+<legend>Noticia</legend><br/>
+<label>Titulo : </label>
+<input type='text' name='titu'><br/>
+<label> Noticia :</label>
+<textarea name='noticia'>Insira sua noticia</textarea>
+<input type="file" name ="imagem"/>
+Ou <input type="text" name ="imagem1"/>
+<input type='submit' value='Enviar ' />
+</fieldset>
+</form>
+
