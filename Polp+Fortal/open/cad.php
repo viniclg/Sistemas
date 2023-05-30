2fb6ee9a9451e49e3fbf7ade7c5cdb3c9359423c
Vinicius P. Matos <124942056+viniclg@users.noreply.github.com>
2023-05-30 20:23:48 -0300

    Delete Polp+Fortal directory

--- Polp+Fortal/open/cad.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="inseriruser.php">
-    <input type="text" name="login" placeholder="Login" required><br>
-    <input type="text" name="nome" placeholder="Nome" required><br>
-    <input type="password" name="senha" placeholder="Senha" required><br>
-    <input type="submit" value="Enviar">
-</form>
